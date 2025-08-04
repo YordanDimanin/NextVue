203dee9ff54ea8cdbd4812bc70ce1884eb073ca1
iordan Dimanin <yordan.dimanin.work@gmail.com>
2025-08-04 14:29:37 +0300

    configure react router and tailwind css

--- NextVue/src/main.tsx
+++ NextVue/src/main.tsx
@@ -3,12 +3,17 @@ import App from './App.tsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './app/store.ts'
-import { BrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+])
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </Provider>
 )
