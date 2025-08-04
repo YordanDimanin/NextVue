7e039b911ff02023ea2b8f2e088eaada152358c9
iordan Dimanin <yordan.dimanin.work@gmail.com>
2025-08-04 20:23:57 +0300

    Fixed layout for small devices

--- NextVue/src/components/Footer.tsx
+++ NextVue/src/components/Footer.tsx
@@ -1,9 +1,9 @@
 
 const Footer = () => {
   return (
-    <div className='flex justify-between px-5'>
-        <img src="../../public/movie.svg" alt="The Movie Database logo" className='w-50 py-2'></img>
-        <p>Developed by: <span className='text-lime-400'>Yordan Dimanin</span></p>
+    <div className='flex justify-around  px-4 py-2'>
+        <img src="../../public/movie.svg" alt="The Movie Database logo" className='w-30 py-2'></img>
+        <p className="text-sm w-[150px]">Developed by: <span className='text-lime-400'>Yordan Dimanin</span></p>
     </div>
   )
 }
