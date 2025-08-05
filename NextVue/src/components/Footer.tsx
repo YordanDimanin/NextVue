ae84b3c0c7b592ccc3e279aacbd6599925dc2f22
iordan Dimanin <yordan.dimanin.work@gmail.com>
2025-08-05 11:25:32 +0300

    style: add text padding, fix footer layout, improve mobile view

--- NextVue/src/components/Footer.tsx
+++ NextVue/src/components/Footer.tsx
@@ -1,9 +1,9 @@
 
 const Footer = () => {
   return (
-    <div className='flex justify-around  px-4 py-2'>
+    <div className='flex justify-between  px-4 py-2'>
         <img src="../../public/movie.svg" alt="The Movie Database logo" className='w-30 py-2'></img>
-        <p className="text-sm w-[150px]">Developed by: <span className='text-lime-400'>Yordan Dimanin</span></p>
+        <p className="text-sm w-[150px] text-right">Developed by: <span className='text-lime-400'>Yordan Dimanin</span></p>
     </div>
   )
 }
--- NextVue/src/pages/Home.tsx
+++ NextVue/src/pages/Home.tsx
@@ -8,7 +8,7 @@ const Home = () => {
       <NavBar />
       <main className='flex flex-grow flex-col items-center justify-center'>
         <h1 className='sm:text-[32px] font-bold text-2xl'>Welcome to <span className='text-lime-400'>NextVue</span></h1>
-        <p className='leading-8 sm:text-xl text-md text-center sm:max-w-[500px] max-w-[400px] pb-10 pt-2'>NextVue is your personal movie companion — helping you discover great films based on your favorite genres. </p>
+        <p className='leading-8 sm:text-xl text-md text-center sm:max-w-[500px] max-w-[400px] pb-10 pt-2 pl-4 pr-4'>NextVue is your personal movie companion — helping you discover great films based on your favorite genres. </p>
         <Button className='sm:text-2xl sm:py-6 sm:px-12 text-xl py-4 px-8 bg-lime-400 border-2 border-lime-400 text-primary-black rounded-lg font-semibold transition transform duration-300 hover:text-lime-400 hover:bg-primary-light-gray hover:border-2 hover:border-lime-400'>Find Movie</Button>
       </main>
       <Footer />
