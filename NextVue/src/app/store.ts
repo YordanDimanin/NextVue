2b51af7668462e52ae124a730c3e32ac9ed4d70f
YordanDimanin <yordan.dimanin.work@gmail.com>
2025-08-11 13:51:32 +0300

    Refactor: api.ts and Filter page

--- NextVue/src/app/store.ts
+++ NextVue/src/app/store.ts
@@ -1,10 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit'
 import genreReducer from './features/genreSlice'
 import filterReducer from './features/filterSlice'
+import moviesReducer from './features/movieSlice'
 
 export const store = configureStore({
     reducer: {
         genre: genreReducer,
         filter: filterReducer,
+        movies: moviesReducer
     },
 })
--- NextVue/src/pages/Filter.tsx
+++ NextVue/src/pages/Filter.tsx
@@ -1,44 +1,59 @@
-import NavBar from "../components/NavBar"
-import Genre from "../components/Genre"
-import FilterBy from "../components/FilterBy"
-import Button from "../components/Button"
-import Footer from "../components/Footer"
-import { Link } from "react-router-dom"
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
-import { fetchMovies } from "../api/api"
-import { useSelector } from "react-redux"
+import NavBar from '../components/NavBar';
+import Genre from '../components/Genre';
+import FilterBy from '../components/FilterBy';
+import Button from '../components/Button';
+import Footer from '../components/Footer';
+
+import { fetchMovies } from '../api/api';
+import { setMovies } from '../app/features/movieSlice';
+
 
 const Filter = () => {
+  const genre = useSelector((state: any) => state.genre.genre);
+  const filter = useSelector((state: any) => state.filter.filter);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const genre = useSelector((state : any) => state.genre.genre);
-  const filter = useSelector((state : any) => state.filter.filter);
+  const handleClick = async () => {
+    const movies = await fetchMovies(genre, filter);
+    dispatch(setMovies(movies));
+    navigate('/result');
+  };
 
 
   return (
     <div className="flex flex-col min-h-screen">
       <NavBar />
 
-      <main className='flex flex-grow flex-col items-center justify-start p-4 pt-20 md:justify-center'>
+      <main className="flex flex-grow flex-col items-center justify-start p-4 pt-20 md:justify-center">
+        <h1 className="sm:text-[38px] font-bold text-2xl pb-10">
+          <span className="text-lime-400">Select Movie</span> Filters
+        </h1>
 
-      <h1 className="sm:text-[38px] font-bold text-2xl pb-10"><span className='text-lime-400'>Select Movie</span> Filters</h1>
+        <div>
+          <p className="sm:text-xl font-semibold">Filter By</p>
+          <FilterBy />
+        </div>
 
-      <div>
-        <p className="sm:text-xl font-semibold">Filter By</p>
-        <FilterBy />
-      </div>
+        <div>
+          <p className="sm:text-xl font-semibold">Genre</p>
+          <Genre />
+        </div>
 
-      <div>
-        <p className="sm:text-xl font-semibold">Genre</p>
-        <Genre />
-      </div>
-
-      <Link to="/result"><Button onClick={() => {fetchMovies(genre, filter)}} className='sm:text-2xl m-8 sm:py-6 sm:px-10 text-xl py-4 px-8 bg-lime-400 border-2 border-lime-400 text-primary-black rounded-lg font-semibold transition transform duration-300 hover:text-lime-400 hover:bg-primary-light-gray hover:border-2 hover:border-lime-400'>Find Movie</Button></Link>
-
+        <Button
+          onClick={handleClick}
+          className="sm:text-2xl m-8 sm:py-6 sm:px-10 text-xl py-4 px-8 bg-lime-400 border-2 border-lime-400 text-primary-black rounded-lg font-semibold transition transform duration-300 hover:text-lime-400 hover:bg-primary-light-gray hover:border-2 hover:border-lime-400"
+        >
+          Find Movie
+        </Button>
       </main>
 
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Filter
+export default Filter;
