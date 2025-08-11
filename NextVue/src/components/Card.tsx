4de3ad5023b6677fff5a5f8d7168b8b7f5f6e720
YordanDimanin <yordan.dimanin.work@gmail.com>
2025-08-11 16:39:20 +0300

    Fixed card colors

--- NextVue/src/components/Card.tsx
+++ NextVue/src/components/Card.tsx
@@ -1,6 +1,6 @@
 const Card = ({ img, title, description, releaseDate }) => {
   return (
-    <div className="flex flex-col items-center w-full bg-white border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 dark:bg-gray-800 md:flex-row md:max-w-4xl">
+    <div className="flex flex-col items-center w-full border rounded-lg shadow-sm border-gray-700 bg-gray-800 md:flex-row md:max-w-4xl">
       
       {/* Image container */}
       <div className="w-full md:w-96 flex-shrink-0">
@@ -13,9 +13,9 @@ const Card = ({ img, title, description, releaseDate }) => {
 
       {/* Text content */}
       <div className="flex flex-col justify-between p-4 md:p-8 leading-normal w-full">
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 md:text-3xl dark:text-slate-200">{title}</h5>
-        <p className="mb-3 font-normal text-gray-700 md:text-lg dark:text-gray-300 leading-loose">{description}</p>
-        <p className="font-bold text-gray-500 dark:text-gray-300">Release Date: {releaseDate}</p>
+        <h5 className="mb-2 text-2xl font-bold tracking-tight md:text-3xl text-slate-200">{title}</h5>
+        <p className="mb-3 font-normal md:text-lg text-gray-300 leading-loose">{description}</p>
+        <p className="font-bold text-gray-300">Release Date: {releaseDate}</p>
       </div>
     </div>
   );
